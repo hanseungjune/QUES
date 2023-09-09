4848a27c51c8fd508952f16b940975b0c84fa521
한승준 <tmdwns950428@naver.com>
2023-09-10 02:34:36 +0900

    feat: 텍스트 및 이미지 드래그 안되게 하기(크로스 브라우징)

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,8 +1,26 @@
-import { styled } from "styled-components";
+import { createGlobalStyle, styled } from "styled-components";
 import DateWeatherCompo from "./components/DateWeatherCompo";
 import IntroduceCompo from "./components/IntroduceCompo";
 import TodosCompo from "./components/TodosCompo";
 import { media } from "./styles/MediaSettingStyle";
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    -webkit-user-select: none; /* Chrome, Safari, Opera */
+    -moz-user-select: none;    /* Firefox */
+    -ms-user-select: none;     /* IE 10+/ Edge */
+    user-select: none;         /* Standard */
+
+    margin: 0;
+    box-sizing: content-box;
+  }
+
+  img {
+    -webkit-user-drag: none;
+    -moz-user-drag: none;
+    -ms-user-drag: none;
+  }
+`;
 
 export const Container = styled.main`
   margin: 10vh auto;
@@ -25,6 +43,7 @@ export const Container = styled.main`
 const Main = () => {
   return (
     <Container>
+      <GlobalStyles />
       {/* 날짜, 날씨 */}
       <DateWeatherCompo />
       {/* 일정 */}
